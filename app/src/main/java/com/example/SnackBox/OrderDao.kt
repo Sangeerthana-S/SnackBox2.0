1050557fa120b3229f96e0ece6e12a0fba3fcd30
Alphin V T <alphoht@gmail.com>
2024-11-13 21:56:58 +0530

    order

---
+++ app/src/main/java/com/example/SnackBox/OrderDao.kt
@@ -0,0 +1,20 @@
+package com.example.SnackBox
+
+
+import androidx.room.*
+
+@Dao
+interface OrderDao {
+
+    @Query("SELECT * FROM order_table WHERE  address= :address")
+    suspend fun getOrderByAddress(address: String): Order?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrder(order: Order)
+
+    @Update
+    suspend fun updateOrder(order: Order)
+
+    @Delete
+    suspend fun deleteOrder(order: Order)
+}
